464fd2413b367436e16f3e8437c8e2bf58aef7e2
amatsukan <luiz.cfj@hotmail.com>
2015-05-04 12:27:50 -0300

    Update

--- gitConf.sh
+++ gitConf.sh
@@ -1,2 +1,10 @@
 git config --global user.name "amatsukan"
 git config --global user.email "luiz.cfj@hotmail.com"
+ls -al ~/.ssh
+ssh-keygen -t rsa -C "luiz.cfj@apilab.ufsc.br"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+sudo apt-get install xclip
+xclip -sel clip < ~/.ssh/id_rsa.pub
+
+
